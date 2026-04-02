887f0609a37a8e31dd4649ab281d691d4afac9f5
Naol <naolmes15062015@gmail.com>
2026-04-02 23:55:57 +0300

    ...

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create.profile.dto';
 @Controller('profiles')
 export class ProfilesController {
   // GET/profiles
   @Get()
-  findAll() {
-    return [];
+  findAll(@Query('location') location: number) {
+    return [{ location }];
   }
+
   // GET/profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+  @Post()
+  create(@Body() CreateProfileDto: CreateProfileDto) {
+    return {
+      name: CreateProfileDto.name,
+      description: CreateProfileDto.description,
+    };
+  }
   // POST/profiles
   // PUT/profiles/:id
   // DELETE/profiles/:id
